915d639fd673200d964ef63f5dc37f1a3fc5c5c6
Veri Play <aleksander018993@gmail.com>
2023-08-07 22:12:47 +0200

    dodanie kolejnego znaku do Docs ?

--- pages/index.tsx
+++ pages/index.tsx
@@ -56,7 +56,7 @@ export default function Home() {
 						target="_blank"
 						rel="noopener noreferrer">
 						<h2>
-							Docs! <span>-&gt;</span>
+							Docs!? <span>-&gt;</span>
 						</h2>
 						<p>
 							Find in-depth information about Next.js features and&nbsp;API.
